f22b907870848f18926581ac1a06b1989f1d5645
Mahmud Hasan <mahmudhasan1290@gmail.com>
2025-07-05 13:28:15 -0400

    Create redeploy-site.sh

---
+++ redeploy-site.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# kill any existing tmux server and sessions
+tmux kill-server
+
+# go to the project directory
+cd ~/mlh-pe-portfolio-site || exit 1
+
+# update code from GitHub main branch
+git fetch && git reset origin/main --hard
+
+# enter virtual environment & install dependencies
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+# start a new detached tmux session named deploy-site
+tmux new-session -d -s deploy-site "cd ~/mlh-pe-portfolio-site && source venv/bin/activate && flask run --host=0.0.0.0"
