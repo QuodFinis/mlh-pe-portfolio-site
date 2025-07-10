67bccd341b67208ac68bf2b81a802bac7cd3a990
Mahmud Hasan <mahmudhasan1290@gmail.com>
2025-07-10 18:57:00 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -9,6 +9,9 @@ cd ~/mlh-pe-portfolio-site || exit 1
 # update code from GitHub main branch
 git fetch && git reset origin/main --hard
 
+# give redeploy-site exec perms
+chmod +x redeploy-site.sh
+
 # enter virtual environment & install dependencies
 source venv/bin/activate
 pip install -r requirements.txt
