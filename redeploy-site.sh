8ac4d1e6f9da78bcebfe8887e93d232795073a9b
Mahmud Hasan <mahmudhasan1290@gmail.com>
2025-08-10 21:16:17 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,18 +2,17 @@
 
 # Enhanced redeployment script with no-op if already up-to-date
 exec > >(tee -a redeploy-site.log) 2>&1
-set -e  # Exit immediately if any command fails
 
 cd ~/mlh-pe-portfolio-site || exit 1
 
-# Check if updates are available
-git fetch
-HEADHASH=$(git rev-parse HEAD)
-UPSTREAMHASH=$(git rev-parse origin/main)
+# Reset local repo to match main branch
+git fetch && git reset origin/main --hard
 
-# Docker deployment
-sudo docker compose -f compose.prod.yaml down
-sudo docker compose -f compose.prod.yaml up -d --build
+# spin containers down to prevent out of memory issues
+docker compose -f docker-compose.prod.yml down
+
+# Rebuild and start container
+docker compose -f docker-compose.prod.yml up -d --build
 
 # Warm up server
 echo "Waiting for server to start..."
